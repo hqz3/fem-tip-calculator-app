a2abc60c6bc6465c7a56461bd3a3c30d5344e867
hqz3 <heqzheng@gmail.com>
2023-05-18 20:49:20 -0400

    Helper functions to keep code DRY

---
+++ src/handleZeroError.ts
@@ -0,0 +1,15 @@
+export function showZeroError(inputEl: HTMLInputElement) {
+  const zeroErrorEl = inputEl
+    .closest("label")
+    ?.querySelector(".zero") as HTMLElement;
+  inputEl.classList.add("error");
+  zeroErrorEl.classList.add("show");
+}
+
+export function hideZeroError(inputEl: HTMLInputElement) {
+  const zeroErrorEl = inputEl
+    .closest("label")
+    ?.querySelector(".zero") as HTMLElement;
+  inputEl.classList.remove("error");
+  zeroErrorEl.classList.remove("show");
+}
