d808c172c2352452cf9d1afa87b4b7399985450e
hqz3 <heqzheng@gmail.com>
2023-05-18 20:49:32 -0400

    Simplify logic

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { hideZeroError, showZeroError } from "./handleZeroError";
+
 let bill: number = 0;
 let people: number = 0;
 let tipPercent: number = 0;
@@ -6,6 +8,9 @@ let tipPerPerson: number = 0;
 let totalPerPerson: number = 0;
 
 const billInput = document.querySelector(".bill__input") as HTMLInputElement;
+const peopleInput = document.querySelector(
+  ".people__input"
+) as HTMLInputElement;
 
 const tipButtons = document.querySelectorAll(
   ".tip__percent"
@@ -15,43 +20,19 @@ const tipCustomInput = document.querySelector(
   ".tip__custom"
 ) as HTMLInputElement;
 
-const peopleInput = document.querySelector(
-  ".people__input"
-) as HTMLInputElement;
-
-const tipCalculationPriceEl = document.querySelector(
+const tipAmountEl = document.querySelector(
   ".calculation__tip .calculation__price"
 ) as HTMLElement;
-const totalCalculationPriceEl = document.querySelector(
+const totalAmountEl = document.querySelector(
   ".calculation__total .calculation__price"
 ) as HTMLElement;
 
-const billZeroErrorEl = billInput
-  .closest("label")
-  ?.querySelector(".zero") as HTMLElement;
-
-const peopleZeroErrorEl = peopleInput
-  .closest("label")
-  ?.querySelector(".zero") as HTMLElement;
-
 function calculate() {
   // Show error if either bill or people is zero
-  if (!bill) {
-    billInput.classList.add("error");
-    billZeroErrorEl.classList.add("show");
-    return;
-  } else {
-    billInput.classList.remove("error");
-    billZeroErrorEl.classList.remove("show");
-  }
-  if (!people) {
-    peopleInput.classList.add("error");
-    peopleZeroErrorEl.classList.add("show");
-    return;
-  } else {
-    peopleInput.classList.remove("error");
-    peopleZeroErrorEl.classList.remove("show");
-  }
+  if (!bill) return showZeroError(billInput);
+  else hideZeroError(billInput);
+  if (!people) return showZeroError(peopleInput);
+  else hideZeroError(peopleInput);
 
   // Calculate the tip per person and total per person
   const totalPerPersonBeforeTip = bill / people;
@@ -61,9 +42,18 @@ function calculate() {
 }
 
 function renderCalculation() {
-  tipCalculationPriceEl.innerHTML = `$${tipPerPerson.toFixed(2)}`;
-  totalCalculationPriceEl.innerHTML = `$${totalPerPerson.toFixed(2)}`;
+  tipAmountEl.innerHTML = `$${tipPerPerson.toFixed(2)}`;
+  totalAmountEl.innerHTML = `$${totalPerPerson.toFixed(2)}`;
 }
+
+[billInput, peopleInput].forEach((input, index) => {
+  input.addEventListener("input", (e: Event) => {
+    let val = (e.currentTarget as HTMLInputElement).value;
+    if (index === 0) bill = parseFloat(val);
+    else people = parseInt(val);
+    calculate();
+  });
+});
 
 function clearTips() {
   tipButtons.forEach((button) => button.classList.remove("active"));
@@ -89,15 +79,6 @@ tipCustomInput?.addEventListener("input", (e: Event) => {
   calculate();
 });
 
-[billInput, peopleInput].forEach((input, index) => {
-  input.addEventListener("input", (e: Event) => {
-    let val = (e.currentTarget as HTMLInputElement).value;
-    if (index === 0) bill = parseFloat(val);
-    else people = parseInt(val);
-    calculate();
-  });
-});
-
 // Reset
 const resetButton = document.querySelector(
   ".calculation__reset"
@@ -115,6 +96,6 @@ resetButton?.addEventListener("click", () => {
 
   billInput.value = "";
   peopleInput.value = "";
-  tipCalculationPriceEl.innerHTML = "$0.00";
-  totalCalculationPriceEl.innerHTML = "$0.00";
+  tipAmountEl.innerHTML = "$0.00";
+  totalAmountEl.innerHTML = "$0.00";
 });
