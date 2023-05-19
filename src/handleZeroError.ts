7f570e2b3ea06b6a2d77d2be43c4045bea5ddd0e
hqz3 <heqzheng@gmail.com>
2023-05-19 01:48:20 -0400

    Remove nested div inside label

--- src/handleZeroError.ts
+++ src/handleZeroError.ts
@@ -1,15 +1,17 @@
 export function showZeroError(inputEl: HTMLInputElement) {
   const zeroErrorEl = inputEl
-    .closest("label")
-    ?.querySelector(".zero") as HTMLElement;
+    .closest("div")
+    ?.parentNode?.querySelector(".zero") as HTMLElement;
+
   inputEl.classList.add("error");
   zeroErrorEl.classList.add("show");
 }
 
 export function hideZeroError(inputEl: HTMLInputElement) {
   const zeroErrorEl = inputEl
-    .closest("label")
-    ?.querySelector(".zero") as HTMLElement;
+    .closest("div")
+    ?.parentNode?.querySelector(".zero") as HTMLElement;
+
   inputEl.classList.remove("error");
   zeroErrorEl.classList.remove("show");
 }
